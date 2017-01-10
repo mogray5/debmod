5d0a633a0044b21e4800555808741cc38c599889
Wayne Gray <mogray5@hotmail.com>
2017-01-09 22:26:37 -0500

    Update domod.sh

--- domod.sh
+++ domod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./debmod -dist="mmrepo" -dbhost="localhost" -dbport="5432" -dbpwd="Hendk387yrhdjue!" -source ~/temp -build ~/build -pkg $1
+./debmod -dist="mmrepo" -dbhost="localhost" -dbport="5432" -dbpwd="mypassword" -source ~/temp -build ~/build -pkg $1
 
 #./debmod -dist="mmrepo" -pkg="$2" -source ~/temp
