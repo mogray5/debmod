0642be4a1afa82304b61134f3b1e2b74d48fbbe4
Wayne Gray <mogray5@hotmail.com>
2017-01-09 22:25:45 -0500

    Update dometa.sh

--- dometa.sh
+++ dometa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./debmod -dist="mmrepo" -dbhost="localhost" -dbport="5432" -dbpwd="Hendk387yrhdjue!" -source ~/temp -build ~/build -buildmode="meta" -pkg $1
+./debmod -dist="mmrepo" -dbhost="localhost" -dbport="5432" -dbpwd="mypassword" -source ~/temp -build ~/build -buildmode="meta" -pkg $1
